3244441cb1bb055522d44c0096f7b3747591def0
Almira Khafizova <mimirka80@gmail.com>
2026-06-27 10:35:15 +0200

    Initial Commit

---
+++ BeenThereBerlin/BeenThereBerlinApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeenThereBerlinApp.swift
+//  BeenThereBerlin
+//
+//  Created by Almira Khafizova on 27.06.26.
+//
+
+import SwiftUI
+
+@main
+struct BeenThereBerlinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
