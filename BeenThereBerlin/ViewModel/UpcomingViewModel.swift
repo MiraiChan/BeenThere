2268dc982b493f707501332195e63bcc83e8fe2e
Almira Khafizova <mimirka80@gmail.com>
2026-07-04 19:59:34 +0200

    Feat: add UpcomingViewModel

---
+++ BeenThereBerlin/ViewModel/UpcomingViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  UpcomingViewModel.swift
+//  BeenThereBerlin
+//
+//  Created by Almira Khafizova on 03.07.26.
+//
+
+import SwiftUI
+import SwiftData
+
+@Observable
+final class UpcomingViewModel {
+  var showingAddSheet = false
+  func filteredShows(_ shows: [Show]) -> [Show] {
+    shows
+      .filter { $0.status ==.upcoming }
+      .sorted { $0.date < $1.date }
+  }
+  func delete(_ show: Show, context: ModelContext) {
+    context.delete(show)
+  }
+}
