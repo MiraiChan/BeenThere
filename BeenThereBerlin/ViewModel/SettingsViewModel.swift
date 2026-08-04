f4522bd450d8ad78d711a62df082811108e6fed4
Almira Khafizova <almira-garipova@mail.ru>
2026-08-04 23:44:36 +0200

    Feat: add new file SettingsViewModel

---
+++ BeenThereBerlin/ViewModel/SettingsViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  SettingsViewModel.swift
+//  BeenThereBerlin
+//
+//  Created by Almira Khafizova on 04.08.26.
+//
+
+import SwiftData
+import UserNotifications
+import Foundation
+
+@Observable
+final class SettingsViewModel {
+  var notificationsEnabled = false
+  
+}
